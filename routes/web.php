3e2e0b5a3c66b4c05b585ad14ecc20364a128edb
ctkai01 <quangnam11032001@gmail.com>
2022-12-04 14:55:45 +0700

    vercel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PriceController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+URL::forceScheme('https');
 Route::get('/', [PriceController::class, 'getPrice']);
 Route::get('/change', [PriceController::class, 'getPriceChange'])->name('change');
 Route::post('/change', [PriceController::class, 'changePrice']);
